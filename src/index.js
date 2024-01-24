75cecd4a6c660c6d49009bc85755792681793d1f
Pchaozz <polsh2001.1@gmail.com>
2024-01-25 00:38:33 +0100

    Update
    Ningun video explica absolutamente nada asi que me toca copiar y despues enterarme de que esta pasando

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 require('dotenv').config();
 
-const { token } = process.env.TOKEN;
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
+const {Client, GatewayIntentBits, Collection} = require('discord.js');
+const fs = require("fs");
+const { token } = process.env;
 
-const client = Client({intents: GatewayIntentBits.Guilds});
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions,
+    ]
+})
+
 client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readFileSync(commandsPath).filter((file) => file.endsWith(".js"));
 
-const functionFolders = fs.readdirSync('./src/functions');
-for (const folder of functionFolders) {
-    const functionFiles = fs
-    .readdirSync(`./scr/function/${folder}`)
-    .filter(file => file.endsWith('.js'));
-    for (const file of functionFiles) 
-        require(`./functions/${folder}/${file}`)(client);
+for(const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command)
+    } else {
+        console.log(`Cuidao, el comando ${filePath} no tiene la propiedad "data" o "execute"`)
+    }
 }
+
+client.login(token)
